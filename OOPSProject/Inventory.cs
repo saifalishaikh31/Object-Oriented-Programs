f2a948544dd3e81fb3d586fce7c041bc2dde5979
saifalishaikh31 <rockyasaifalishaikh@gmail.com>
2022-02-22 18:03:24 +0530

    [Saif Ali] Add : JSON Inventory Data Management Added

---
+++ OOPSProject/Inventory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSProject
+{
+    public class Inventory
+    {
+        
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Weight { get; set; }
+    
+    }
+}
---
+++ OOPSProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InventoryManagement inventoryManagement = new InventoryManagement();
+            inventoryManagement.ReadData(@"D:\BridgeLabz\Object-Oriented-Programs\OOPSProject\Inventory.json");
+            Console.ReadKey();
+        }
+    }
+}
